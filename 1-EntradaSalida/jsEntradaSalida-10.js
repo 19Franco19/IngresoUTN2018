adff1c77d56b3a069ad25a6f67fe2eb1849b9383
19Franco19 <36230567+19Franco19@users.noreply.github.com>
2018-02-08 10:12:38 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,6 +3,12 @@ Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
-{
-	
+{var importe;
+    var nuevoImporte;
+    var descuento;
+    
+    importe= parseFloat(document.getElementById("importe").value);
+    descuento = importe * 0.25;
+    nuevoImporte= importe - descuento;
+    document.getElementById("resultado").value = nuevoImporte.toFixed(2);
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,21 +4,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {var sueldo;
-    var resultado;
+    var NuevoSueldo
     var aumento;
-    var multiplicacion;
-    var division;
-    sueldo= document.getElementById("sueldo").value;
-    sueldo= parseInt (sueldo);
-    multiplicacion= sueldo * 10;
-    multiplicacion= parseInt (multiplicacion) 
-    division= multiplicacion / 100;
-    division= parseInt (division)
-    document.getElementById("resultado").value = resultado;
-    aumento= sueldo + division;
-    aumento = parseInt (aumento);
-    resultado = aumento;
-    resultado = parseInt (resultado);
+    
+    sueldo= parseFloat (document.getElementById("sueldo").value);
+    
+    aumento = sueldo * 0.1;
+    NuevoSueldo= sueldo + aumento;
+    document.getElementById("resultado").value = NuevoSueldo.toFixed(0)
+    
+    
+    
     
    
     
