813ac0ddcbc8829fc6bcb6727617afafe355f5fa
19Franco19 <36230567+19Franco19@users.noreply.github.com>
2018-02-07 23:47:05 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -3,7 +3,25 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
-{
+{var sueldo;
+    var resultado;
+    var aumento;
+    var multiplicacion;
+    var division;
+    sueldo= document.getElementById("sueldo").value;
+    sueldo= parseInt (sueldo);
+    multiplicacion= sueldo * 10;
+    multiplicacion= parseInt (multiplicacion) 
+    division= multiplicacion / 100;
+    division= parseInt (division)
+    document.getElementById("resultado").value = resultado;
+    aumento= sueldo + division;
+    aumento = parseInt (aumento);
+    resultado = aumento;
+    resultado = parseInt (resultado);
+    
+   
+    
 
-	
+
 }
