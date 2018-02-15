b1fd0acbc6d789afb6d5a66aeb028e41f551da99
19Franco19 <36230567+19Franco19@users.noreply.github.com>
2018-02-15 12:19:52 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,13 @@
 function Mostrar()
 {
-
+var clave= "utn750"
 var clave = prompt("ingrese el número clave.");
 
+while(clave != "utn750")
+{
+    clave = prompt("Error, ingrese la clave nuevamente")
+    
+}
 
+alert ("Clave Correcta")
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,9 @@
 function Mostrar()
+{  
+   var numero = parseInt(prompt("ingrese un número entre 0 y 10."));
+while( numero < 0 ||  numero > 9 || isNaN(numero))
 {
-
-	var numero = prompt("ingrese un número entre 0 y 10.");
-
-
+ numero =parseInt(prompt("Error. Reingrese un numero entre 0 y 10 ")); 
+}
+document.getElementById("Numero").value = numero;
 }//FIN DE LA FUNCIÓN
