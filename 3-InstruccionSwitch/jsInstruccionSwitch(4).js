8e6884bd518f2618ff91e2e542f31d67ba294be9
19Franco19 <36230567+19Franco19@users.noreply.github.com>
2018-02-15 10:44:04 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -6,51 +6,26 @@ switch (mesDelAño)
  {
 
     case ("Enero"):
-        alert("Tiene 31 dias")
-        break;
+    case("Marzo"):
+    case ("Mayo"):
+    case("Julio"):
+    case("Agosto"):
+    case("Octubre"):
+    case("Diciembre"):
+    alert("Tiene 31 dias")
+    break;
 
     case ("Febrero"):
     alert("Tiene 28 dias")
-       break;
-
-    case("Marzo"):
-    alert("Tiene 31 dias")
     break;
 
-    case("Abril"):
+    
+    default:
     alert("Tiene 30 dias")
     break;
 
-    case ("Mayo"):
-    alert("Tiene 31 dias")
-    break;
-
-    case("Junio"):
-    alert("Tiene 30 dias")
-    break;
-
-    case("Julio"):
-    alert("Tiene 31 dias")
-    break;
-
-    case("Agosto"):
-    alert("Tiene 31 dias")
-    break;
-
-    case("Septiembre"):
-    alert("Tiene 30 dias")
-    break;
-
-    case("Octubre"):
-    alert("Tiene 31 dias")
-    break;
-
-    case("Noviembre"):
-    alert("Tiene 30 dias")
-    break;
-
-    case("Diciembre"):
-    alert("Tiene 31 dias")
+    
+    
  }
 //alert (mesDelAño);
 	
